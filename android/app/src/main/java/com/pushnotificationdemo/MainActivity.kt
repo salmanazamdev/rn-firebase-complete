fd171540f6f8ee8673584922fd42d6342c8d8514
SalmanAzamDev <salmankhach@gmail.com>
2025-08-25 09:52:58 +0500

    renamed the maincomponent name

--- android/app/src/main/java/com/pushnotificationdemo/MainActivity.kt
+++ android/app/src/main/java/com/pushnotificationdemo/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "PushNotificationDemo"
+  override fun getMainComponentName(): String = "pushnotificationdemo"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
